4c9d50ce4504787b7519f7e588dcb08274fa4a01
João Costa Seco <jrcs@fct.unl.pt>
2020-03-04 12:47:52 +0000

    Added one more exercise

--- labclasses/lab1/src/Lab1.java
+++ labclasses/lab1/src/Lab1.java
@@ -3,4 +3,12 @@ public class Lab1 {
     static int identity(int i) {
         return 0;
     }
+
+    // Return the maximum number of the first N numbers in array a
+    static int max(int[] a, int N) {
+        return a[0];
+    }
+
+
+
 }
--- labclasses/lab1/test/Lab1Tests.java
+++ labclasses/lab1/test/Lab1Tests.java
@@ -5,12 +5,29 @@ import static org.junit.Assert.assertEquals;
 public class Lab1Tests {
 
     @Test
-    public void test1() {
+    public void testId1() {
         assertEquals(0,Lab1.identity(0));
     }
 
     @Test
-    public void test2() {
+    public void testId2() {
         assertEquals(1,Lab1.identity(1));
     }
+
+    @Test
+    public void testMax1() {
+        assertEquals(1, Lab1.max(new int[]{1,2,3,4,5,6},1));
+        assertEquals(2, Lab1.max(new int[]{1,2,3,4,5,6},2));
+        assertEquals(4, Lab1.max(new int[]{1,2,3,4,5,6},4));
+        assertEquals(6, Lab1.max(new int[]{1,2,3,4,5,6},6));
+    }
+
+    @Test
+    public void testMax2() {
+        assertEquals(-1, Lab1.max(new int[]{-1,-2,-3,-4,-5,-6},1));
+        assertEquals(-1, Lab1.max(new int[]{-2,-1,-3,-4,-5,-6},2));
+        assertEquals(-1, Lab1.max(new int[]{-2,-3,-1,-4,-5,-6},4));
+        assertEquals(-1, Lab1.max(new int[]{-4,-2,-3,-1,-5,-6},6));
+    }
+
 }
