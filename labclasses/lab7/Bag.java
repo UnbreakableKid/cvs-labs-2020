83c1a48d868ab399581a2fb4328e129a0e23e795
Eduardo Geraldo <e.geraldo@campus.fct.unl.pt>
2020-04-23 17:31:56 +0100

    Lab7

---
+++ labclasses/lab7/Bag.java
@@ -0,0 +1,34 @@
+/*
+Starting point for exercise of Lab Session 6.
+CVS course - Integrated Master in Computer Science and Engineering
+@FCT UNL Eduardo Geraldo, João Costa Seco 2019
+*/
+
+class Bag {
+
+    int[] array;
+    int nelems;
+    int capacity;
+
+    /*
+     * Initializaes the underlying qarray with the length size.
+     */
+    public Bag(int size) {}
+
+    /*
+     * Inserts the integer v into this bag.
+     */
+    public void insert(int v) {}
+
+    /*
+     * Returns the index of the first occurrence of f in this bag. If the value
+     * v is not in this bag, this operation returns -1.
+     */
+    public int indexOf(int elem) {}
+
+    /*
+     * Removes the element at index idx from this bag.
+     */
+    public void remove(int idx) {}
+
+}
