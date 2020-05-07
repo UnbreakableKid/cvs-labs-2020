bab14f0bb1c6d8ef2c04fa917ab1466343cc8dd1
Eduardo Geraldo <e.geraldo@campus.fct.unl.pt>
2020-05-07 16:07:15 +0100

    Lab9

---
+++ labclasses/lab9/MRSWLock.java
@@ -0,0 +1,33 @@
+/* For this lab, you'll have to implement and verify  a readers-writer lock,
+   also known as a multiple readers single writer lock. As the name implies,
+   multiple readers can acquire the lock with reading rights, however, it can
+   only be held by one writer. Furthermore, it if the lock has been acquired for
+   reading, it cannot be acquired for writing and vice-versa.
+*/
+
+class MRSWLock {
+	
+	/* 
+	 * Aquires this lock for reading, after successfully returned from this
+	 * method, the caller has reading rights on the state protected by this
+	 * lock.
+	 */
+	public void readLock ();
+	
+	/* 
+	 * Releases the reading lock.
+	 */
+	void readUnlock ();
+	
+	/*
+	 * Aquires this lock for writing, after successfully returned from this
+	 * method, the caller has writing rights on the state protected by this
+	 * lock.
+	 */
+	void writeLock ();
+
+	/*
+	 * Releases the writing lock.
+	 */
+	void writeUnlock ();
+}
